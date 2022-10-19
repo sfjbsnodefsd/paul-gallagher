b150938e03cb905f06001c675fd523f06b9f9ce1
sfjbssept <sfjbssept@gmail.com>
2022-10-19 15:39:37 +0000

    added kafka streams

--- Kafka/consumer/index.js
+++ Kafka/consumer/index.js
@@ -1,1 +1,17 @@
-console.log("consumer...");
+console.log("consumer")
+import kafka from "node-rdkafka";
+
+const consumer = kafka.KafkaConsumer({
+    'group.id': "kafka",
+    "metadata.broker.list":"localhost:9092"
+},{})
+
+consumer.connect()
+
+consumer.on("ready", () =>{
+    console.log('consumer ready....')
+    consumer.subscribe(['test'])
+    consumer.consume()
+}).on('data' ,(data) =>{
+    console.log(`message received : ${data.value}`)
+})
---
+++ Kafka/producer/index.js
@@ -0,0 +1,23 @@
+console.log("producer")
+import kafka from "node-rdkafka";
+
+const stream = kafka.createWriteStream(
+  {
+    "metadata.broker.list": "localhost:9092",
+  },
+  {},
+  { topic: "test" }
+);
+
+function queueMessage(){
+    const success = stream.write(Buffer.from("Hey my name is Pauly"))
+    if(success){
+      console.log("message published success")
+    }else {
+      console.log("something went wrong")
+    }
+}
+
+setInterval(()=>{
+        queueMessage()
+},3000)
