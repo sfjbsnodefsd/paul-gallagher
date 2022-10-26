136a0b20bd1d0db675f0adc544985f3f2429feb3
sfjbssept <sfjbssept@gmail.com>
2022-10-26 10:25:58 +0000

    updated dbs

--- PensionManagmentSystem/processPensionModule/index.js
+++ PensionManagmentSystem/processPensionModule/index.js
@@ -1,22 +1,53 @@
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
-const pensionerSchema = require('./pensionerSchema');
-const csvtojson = require('csvtojson')
+const pensionerSchema = require('../pensionerDetailsModule/pensionerSchema');
+const csv = require('csvtojson')
+const isAuthenticated = require("../isAuthenticated")
+app.use(express.json());
 
 
 mongoose.connect(
-    "mongodb://localhost:27017/pension-process",
+    "mongodb://localhost:27017/pensioner-details",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     },
     () => {
-      console.log(`pension-process DB is Connected`);
+      console.log(`pensioner-details DB is Connected`);
     }
   );
   
+  const  createPension = (pensioners, aadhaar) => {
+    let newPensionAmmount = 0;
+    //let bankServiceCharge = 0;
+    
+    //for(t=0; t<pensioners.length; ++t) {
+        if(pensioners[0].SelfOrFamily=="self"){
+            newPensionAmmount = ((pensioners[0].Salary + pensioners[0].Allowances)*.8);
+        }
+        else if (pensioners[0].SelfOrFamily=="family"){
+            newPensionAmmount=((pensioners[0].Salary + pensioners[0].Allowances)*.5);
+        }
+        
+        if(pensioners[0].bank_detail.PublicOrPrivate=="public") {
+            bankServiceCharge = 500;
+        }
+        else {
+            bankServiceCharge = 550;
+        }
+        const newPension = new Pensioner({
+            pensioners,
+            //user: aadhaar,
+            pensionAmmount:newPensionAmmount,
+            bankServiceCharge:500
+        });
+        updatedPension = newPensionAmmount;
 
+        newPension.save();
+        return newPension;
+
+    }
   
   
  
@@ -33,20 +64,20 @@ mongoose.connect(
     return res.json(singlePensioner)
   }) */
   
+ app.get("/pensioner/:Aadhaar",isAuthenticated, async (req, res) => {
+  const aadhaar = req.params.aadhaar;
 
-  app.post("/pensioner/:aadhaar", async (req, res) => {
-    const aadhaar = req.params.aadhaar;
-  
-    try {
-      const pensioners = await pensionerSchema.findOne({ Aadhaar: aadhaar }, req.body);
-      if (!pensioners) {
-        return res.status(404).send('Invalid pensioner detail provided, please provide valid detail.')
-      }
-      res.json(pensioners);
-    } catch (err) {
-      return res.json(err)
+  try {
+    const pensioner = await pensionerSchema.findOne({ Aadhaar: aadhaar }, req.body);
+    if (!pensioner) {
+      return res.status(404).send('Invalid pensioner detail provided, please provide valid detail.')
     }
-  });
+    res.json(pensioner);
+
+  } catch (err) {
+    return res.json(err)
+  }
+}); 
 
 
 
